1501b42c36c4da5f7703fd05f44f3c22ee9a87fc
Mehul Pandey <54852991+mehulp1612@users.noreply.github.com>
2021-12-30 01:56:29 +0530

    Create MainApp.java

---
+++ MainApp.java
@@ -0,0 +1,10 @@
+
+public class MainApp{
+
+	public static void main(String[] args)
+	{
+		Inventory obj = new Inventory();
+		obj.setVisible(true);					//Makes the frame visible to the user (default = false)	
+	}
+
+}
